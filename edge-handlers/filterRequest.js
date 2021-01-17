dcee755ac7e474aa32b4de711cc235eb4b27820c
Taylor C. MacDonald <taylor@helloample.com>
2021-01-17 16:22:02 -0500

    Names

--- edge-handlers/filterRequest.js
+++ edge-handlers/filterRequest.js
@@ -1,4 +1,4 @@
+// Log every incoming request URL
 export function onRequest(event) {
   console.log(`Incoming request for ${event.request.url}`);
-  // event.replaceResponse(() => fetch("https://www.netlify.com/"));
 }
