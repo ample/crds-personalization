4ab47468f995969cb0f8d2c187a884cabc3bfdf9
Taylor C. MacDonald <taylor@helloample.com>
2021-02-01 23:06:52 -0500

    .

---
+++ edge-handlers/transformResponse.js
@@ -0,0 +1,22 @@
+export function onRequest(event) {
+  event.replaceResponse(async () => {
+    const originResponse = await fetch(event.request);
+    const transformation = new TransformStream({
+      flush(controller) {
+        const encoder = new TextEncoder();
+        const buf = encoder.encode(
+          "<p>Served from a Netlify Edge Handler</p>");
+        controller.enqueue(buf);
+      },
+    });
+	
+    const transformedBody = originResponse.body.pipeThrough(transformation);
+    const headers = {
+      'Content-Type': 'text/html'
+    };
+
+    return new Response(transformedBody, {
+      headers
+    });
+  });
+}
