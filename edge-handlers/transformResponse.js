7a2aeb133236f033b63f15a487ac48414953bfc4
Taylor C. MacDonald <taylor@helloample.com>
2021-02-01 23:16:21 -0500

    .

--- edge-handlers/transformResponse.js
+++ edge-handlers/transformResponse.js
@@ -1,21 +1,16 @@
+const parse = require('node-html-parser').parse;
+
 export function onRequest(event) {
   event.replaceResponse(async () => {
     const originResponse = await fetch(event.request);
-    const transformation = new TransformStream({
-      flush(controller) {
-        const encoder = new TextEncoder();
-        const buf = encoder.encode(
-          "<p>Served from a Netlify Edge Handler</p>");
-        controller.enqueue(buf);
-      },
-    });
-	
-    const transformedBody = originResponse.body.pipeThrough(transformation);
+
     const headers = {
       'Content-Type': 'text/html'
     };
+    const root = parse(originResponse);
+    const body = root.querySelector('body');
 
-    return new Response(transformedBody, {
+    return new Response(body, {
       headers
     });
   });
